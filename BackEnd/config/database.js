6141b50e4ba666e5d7f799d7e3f91c44649947dc
adebayo-glitch <pillarbasetech@gmail.com>
2024-10-06 18:12:43 -0400

    update the db config

--- BackEnd/config/database.js
+++ BackEnd/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-    const dbURI = "mongodb+srv://cooverotech:wFY2A2pGICTw88xF@cluster0.cyiwe.mongodb.net/portaldb?retryWrites=true&w=majority&appName=Cluster0"
+    const dbURI = "mongodb+srv://pillarbasetech:l69a3SHdFAH9IeH8@cluster0.zjuv6.mongodb.net/studentmanagementdb?retryWrites=true&w=majority&appName=Cluster0"
     const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
     try {
         mongoose.connect(dbURI, clientOptions)
